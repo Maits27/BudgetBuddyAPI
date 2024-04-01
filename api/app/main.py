eac6bea51027967ccc3bf40ca359be4a1405b0ed
Maits27 <maitiurlo@gmail.com>
2024-04-01 17:23:37 +0200

    cambioorden

--- api/app/main.py
+++ api/app/main.py
@@ -47,15 +47,15 @@ def delete_user(email: str, db: Session = Depends(get_db)):
 ##################################    GASTOS    ##################################
 ##################################################################################
 
-@app.post('/{user_id}/gastos/', response_model=list[Gasto])
+@app.post('/gastos/{user_id}/', response_model=list[Gasto])
 def create_gasto(gasto: GastoCreate, db: Session = Depends(get_db)):
     return crud.create_gasto(db, gasto)
 
-@app.get('/{user_id}/gastos/', response_model=list[Gasto])
+@app.get('/gastos/{user_id}/', response_model=list[Gasto])
 def read_gastos_by_user(user_id: str, db: Session = Depends(get_db)):
     return crud.get_gastos_by_user(db, user_id)
 
-@app.delete('/{user_id}/gastos/', response_model=list[Gasto])
+@app.delete('/gastos/{user_id}/', response_model=list[Gasto])
 def delete_all_gastos_by_user(user_id: str, db: Session = Depends(get_db)):
     return crud.delete_all_gastos_by_user(db, user_id)
 
