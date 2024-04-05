e5f18edd5a1e812cc6f93d8df6808c593c876d26
Maits27 <maitiurlo@gmail.com>
2024-04-05 19:56:45 +0200

    cambio upload

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, UploadFile
+from fastapi import FastAPI, Depends, HTTPException, UploadFile, status
 from fastapi.responses import FileResponse
 from pathlib import Path
 from mimetypes import guess_extension
@@ -60,19 +60,23 @@ def get_profile_image(email: str, db: Session = Depends(get_db)):
     else:
         return FileResponse("/budgetbuddy_api/images/start_icon.png", filename="start_icon.png")
 
-@app.put('/profile/{email}', response_model=User)
-def update_profile_image(email: str, profile_image: UploadFile, db: Session = Depends(get_db)):
-    if not (user:= crud.get_user(db, email)):
-        raise HTTPException(status_code=404, detail="User not found")  
-    if profile_image.content_type not in VALID_IMAGE_TYPES:
-        raise HTTPException(status_code=400, detail="Invalid image type")
+@app.put("/profile/{email}", tags=["Users"],
+         status_code=status.HTTP_204_NO_CONTENT,
+         responses={404: {"description": "User doesn't exists."}, 400: {"description": f"File is not a valid image file. Valid types: {', '.join(VALID_IMAGE_TYPES)}"}})
+async def set_user_profile_image(file: UploadFile, email: str, db: Session = Depends(get_db)):
+    if not (user := crud.get_user(db, email)):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User doesn't exists.")
 
-    file_extension = guess_extension(profile_image.content_type)
-    path = f"/budgetbuddy_api/images/{email}{file_extension}"
-    if crud.set_profile_image(db, email, path):
-        with open(path, "wb") as file:
-            file.write(profile_image.file.read())
-        return user
+    if file.content_type not in VALID_IMAGE_TYPES:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"File is not a valid image file. Valid types: {', '.join(VALID_IMAGE_TYPES)}")
+
+    file_extension = guess_extension(file.content_type)
+    path = f'/budgetbuddy_api/images/{email}{file_extension}'
+
+    if crud.set_profile_image(db, user, path):
+        contents = await file.read()
+        with open(path, 'wb') as f:
+            f.write(contents)
 
 
 ##################################################################################
